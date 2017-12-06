a7089307a3ac919fe834e3685822e6a177bf7509
mrumpf <michael@rumpfonline.de>
2017-12-06 15:16:04 -0700

    Read the files from the classpath

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -3,27 +3,37 @@ package org.superbiz.moviefun;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CsvUtils {
 
-    public static String readFile(String path) {
+    public static String readFileLegacy(String path) {
+        File f = new File(path);
+        String result = "";
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
-
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
+            result = loadFile(new FileInputStream(f));
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static String readFile(String path) {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        InputStream res = cl.getResourceAsStream(path);
+        return loadFile(res);
+    }
+
+    private static String loadFile(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
     }
 
